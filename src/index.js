71f1ee6aa5208446f769fd0cd79c49268bd90170
Kellojo <kellojo1@gmail.com>
2022-10-05 19:24:10 +0200

    updated package

--- src/index.js
+++ src/index.js
@@ -1,54 +1,30 @@
 #!/usr/bin/env node
 
-import {sendRequest} from './RestClient.js';
 import commandLineArgs from 'command-line-args';
+import fetch from "node-fetch";
 import chalk from 'chalk';
-import death from 'death';
 
 let interval = null;
 const args = commandLineArgs([
-    { name: 'startUrl', alias: 'a', type: String },
-    { name: 'startMethod', alias: 'b', type: String },
-    { name: 'startBody', alias: 'c', type: String },
-
-    { name: 'stopUrl', alias: 'd', type: String },
-    { name: 'stopMethod', alias: 'e', type: String },
-    { name: 'stopBody', alias: 'f', type: String },
+    { name: 'url', alias: 'a', type: String },
+    { name: 'interval', alias: 'i', type: Number },
 ]);
 
-// keep node running after code finishes (wait for on death callback)
-if (args.stopUrl) {
-    interval = setInterval(() => { }, 1 << 30);
+
+console.log(chalk.bold("PC Webhooks"));
+
+if (!args.interval) args.interval = 1000;
+
+
+const sendRequest = async () => {
+    console.log(`Sending GET request to ${args.url}`);
+    const response = await fetch(args.url, {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'GET'
+    });
+
+    console.log(`${response.statusText} ${response.status}`);
 }
 
-
-console.log(chalk.bold("PC-Event-Webhooks"));
-
-
-// start request
-if (args.startUrl) {
-    console.log("Sending start request...");
-    sendRequest(
-        args.startUrl,
-        args.startMethod || "GET",
-        args.startBody || "",
-    );
-}
-
-// stop request
-death(() => {
-    clearInterval(interval);
-
-    if (args.stopUrl) {
-        console.log("Waiting for shutdown...");
-
-
-        console.log("Sending shutdown request...");
-        sendRequest(
-            args.stopUrl,
-            args.stopMethod || "GET",
-            args.stopBody || "",
-        );
-    }
-});
-
+sendRequest();
+setInterval(sendRequest, args.interval);
